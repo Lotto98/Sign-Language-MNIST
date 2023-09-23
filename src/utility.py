bb7b87986f7497d738d273ba0782112d1aedc645
Michele Lotto <875922@stud.unive.it>
2023-09-23 18:53:01 +0200

    data retrieval pipeline

--- src/utility.py
+++ src/utility.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 
 import gc
 
-response_conv={n:chr(n+65) for n in range(0,26)}
+import os
+
+response_transform={n:chr(n+65) for n in range(0,26)}
 
 def get_dataset(path:str):
     
@@ -28,7 +30,7 @@ def plot_images(train_X:pd.DataFrame, train_Y:pd.Series, ran:int=666):
         ax.axis("off")
         
         if k<=25:
-            ax.set_title(response_conv[k])
+            ax.set_title(response_transform[k])
             
             if k!=9 and k!=25: #impossible to distinguish J and Z
                 
@@ -40,3 +42,29 @@ def plot_images(train_X:pd.DataFrame, train_Y:pd.Series, ran:int=666):
                 to_show=np.zeros((28,28))
             
             ax.imshow(to_show,cmap='gray')
+            
+def save_dataframes(train_X:pd.DataFrame, test_X:pd.DataFrame,
+                    train_Y:pd.Series, test_Y:pd.Series):
+    
+    dfs_path = os.getcwd()+"/../data/dataframes" 
+    if not os.path.exists(dfs_path):
+        os.makedirs(dfs_path)
+
+    train_X.to_parquet(dfs_path+"/train_X.parquet")
+    train_Y.to_frame().to_parquet(dfs_path+"/train_Y.parquet")
+
+    test_X.to_parquet(dfs_path+"/test_X.parquet")
+    test_Y.to_frame().to_parquet(dfs_path+"/test_Y.parquet")
+
+def load_dataframes():
+    dfs_path = os.getcwd()+"/../data/dataframes" 
+    if not os.path.exists(dfs_path):
+        pass
+    
+    train_X = pd.read_parquet(dfs_path+"/train_X.parquet")
+    train_Y = pd.read_parquet(dfs_path+"/train_Y.parquet").squeeze()
+    
+    test_X = pd.read_parquet(dfs_path+"/test_X.parquet")
+    test_Y = pd.read_parquet(dfs_path+"/test_Y.parquet").squeeze()
+    
+    return train_X, train_Y, test_X, test_Y
