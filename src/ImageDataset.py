886f1b55cbe11323a588367468981111ae193e26
Michele Lotto <875922@stud.unive.it>
2023-10-03 18:00:46 +0200

    added transformations

--- src/ImageDataset.py
+++ src/ImageDataset.py
@@ -12,7 +12,7 @@ import torchvision.transforms as T
 from typing import Tuple
 
 class ImageDataset(Dataset):
-    def __init__(self, dataframe_X:pd.DataFrame, series_Y:pd.Series, transform_dimension:Tuple[int,int], device:str="cuda") -> None:
+    def __init__(self, dataframe_X:pd.DataFrame, series_Y:pd.Series, transform_dimension:Tuple[int,int], data_augmentation_perc:float=0, device:str="cuda") -> None:
         super().__init__()
         
         assert transform_dimension[0] > 0 and transform_dimension[1] > 0, "Resize dimensions should be > 0"
@@ -24,8 +24,22 @@ class ImageDataset(Dataset):
         
         self._X = torch.from_numpy(numpy_X).to(device)
         self._X = T.Resize(transform_dimension, antialias = False)(self._X) # type: ignore
-        
         self._Y = torch.from_numpy(series_Y.to_numpy()).to(device)
+        
+        if data_augmentation_perc != 0:
+            
+            indices_to_transform = torch.randperm(len(self._X))[:int(len(self._X)*(data_augmentation_perc))]
+            
+            indices_to_perspective = indices_to_transform[:len(indices_to_transform)//2]
+            
+            indices_to_rotate = indices_to_transform[len(indices_to_transform)//2:]
+            
+            transformed_perspective = T.RandomPerspective(p=1)(self._X[indices_to_perspective])
+            
+            transformed_rotation = T.RandomRotation(degrees=45)(self._X[indices_to_rotate])
+            
+            self._X = torch.cat((transformed_perspective,transformed_rotation, self._X))
+            self._Y = torch.cat((self._Y[indices_to_perspective], self._Y[indices_to_rotate], self._Y))
         
     def __len__(self) -> int:
         return len(self._X)
@@ -33,6 +47,7 @@ class ImageDataset(Dataset):
     def __getitem__(self, index):
         return self._X[index] , self._Y[index]
     
+    """
     def spit_train_val(self, perc_val_size:float):
         
         if perc_val_size < 0 or perc_val_size > 1:
@@ -43,3 +58,4 @@ class ImageDataset(Dataset):
         train_size -= val_size
 
         return random_split(self, [int(train_size), int(val_size)]) #train_data, val_data 
+    """
