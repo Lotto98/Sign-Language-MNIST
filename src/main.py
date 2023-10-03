dd766ff1ee68733c1fbd32d2923b239d083e012b
Michele Lotto <875922@stud.unive.it>
2023-10-03 12:35:18 +0200

    initial test function

--- src/main.py
+++ src/main.py
@@ -3,12 +3,42 @@ from NeuralNetwork import NeuralNetwork
 from models import Classifier_3, LeNet5
 
 import torch.optim as optim
+import torch.nn as nn
 
-model = Classifier_3()
-optimizer = optim.Adam(model.parameters(),lr=0.0001, amsgrad=True)
+from typing import Tuple
 
-net = NeuralNetwork(model,optimizer,"Classifier_3", model_input_dim = (28,28) )
+from itertools import product
 
-#net.load_model()
+from tqdm import tqdm
 
-net.full_training()
+import os
+
+def single_test(model:nn.Module, model_name:str, batch_size:int, patience:int, lr:float=0.0001, model_input_dim:Tuple[int,int] = (28,28)):
+    
+    optimizer = optim.Adam(model.parameters(),lr=lr, amsgrad=True)
+
+    net = NeuralNetwork(model, optimizer, model_name, model_input_dim, batch_size, patience)
+
+    net.full_training()
+
+def multiple_test(model:nn.Module, model_name:str):
+    
+    lrs = [0.1, 0.001, 0.0001]
+    batch_sizes = [16, 32, 64, 128, 256]
+    patiences = [5, 10, 15, 20]
+    
+    prod=[x for x in product(lrs, batch_sizes, patiences)]
+    
+    bar = tqdm(enumerate(prod), total=len(prod))
+    
+    for i,(lr, batch_size, patience) in bar:
+        
+        os.system('clear')
+        
+        bar.set_description(f"Test with parameters: lr={lr}, batch_size={batch_size}, patience={patience}")
+        
+        name = model_name + f"_test_{i}"
+        
+        single_test(model=model, model_name=name, lr=lr, batch_size=batch_size, patience=patience )
+    
+multiple_test(model=Classifier_3(), model_name="Classifier_3")
